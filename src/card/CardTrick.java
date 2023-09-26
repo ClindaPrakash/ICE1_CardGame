82431f91f15a81e73648533c39b3c6ddc56e6650
clind <clind@10.0.0.75>
2023-09-26 15:52:37 -0400

    Updated card tricks...

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package card;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -15,18 +17,47 @@ public class CardTrick {
     public static void main(String[] args)
     {
         Card[] magicHand = new Card[7];
+        Scanner scanner = new Scanner(System.in);
+        
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            if(i<6) {
+                Card c = new Card();
+                Random rand = new Random();
+                c.setValue(rand.nextInt(12));
+                c.setSuit(Card.SUITS[rand.nextInt(3)]);
+                magicHand[i] = c;
+            } else {
+                // add one luckcard hard code 2,clubs
+                Card c = new Card();
+                c.setValue(2);
+                c.setSuit("Clubs");
+                magicHand[i] = c;
+            }
         }
         
-        //insert code to ask the user for Card value and suit, create their card
+        //e to ask the user for Card value and suit, create their card
+        System.out.print("Enter Card Suit: ");
+        String cardSuit = scanner.nextLine();
+        System.out.print("Enter Card Value: ");
+        Integer cardValue = scanner.nextInt();
+        Card selectedCard = new Card();
+        selectedCard.setValue(cardValue);
+        selectedCard.setSuit(cardSuit);
         // and search magicHand here
-        //Then report the result here
-        // add one luckcard hard code 2,clubs
+        Boolean found = false;
+        for (int i=0; i<magicHand.length; i++) {
+            if(selectedCard.getSuit().equals(magicHand[i].getSuit()) && selectedCard.getValue()== magicHand[i].getValue()) {
+             //Then report the result here 
+             System.out.print("Lucky Winner! Your card was found");
+             found = true;
+            }
+        }
+        if(!found) {
+            System.out.println("Your card was not found");
+        }
+        
     }
     
 }
