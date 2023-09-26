f260fda342e348a0a660b19ca7e39567564230d8
ClindaPrakash <146124922+ClindaPrakash@users.noreply.github.com>
2023-09-26 16:08:35 -0400

    Update CardTrick.java

--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -22,29 +22,17 @@ public class CardTrick {
         
         for (int i=0; i<magicHand.length; i++)
         {
-            if(i<6) {
                 Card c = new Card();
                 Random rand = new Random();
                 c.setValue(rand.nextInt(12));
                 c.setSuit(Card.SUITS[rand.nextInt(3)]);
                 magicHand[i] = c;
-            } else {
-                // add one luckcard hard code 2,clubs
-                Card c = new Card();
-                c.setValue(2);
-                c.setSuit("Clubs");
-                magicHand[i] = c;
-            }
         }
         
         //e to ask the user for Card value and suit, create their card
-        System.out.print("Enter Card Suit: ");
-        String cardSuit = scanner.nextLine();
-        System.out.print("Enter Card Value: ");
-        Integer cardValue = scanner.nextInt();
         Card selectedCard = new Card();
-        selectedCard.setValue(cardValue);
-        selectedCard.setSuit(cardSuit);
+        selectedCard.setValue(2);
+        selectedCard.setSuit("Clubs");
         // and search magicHand here
         Boolean found = false;
         for (int i=0; i<magicHand.length; i++) {
